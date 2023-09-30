fc581faa92f620321107e96ab3f1cf23d6cbb8e4
megan <mrutledg02@gmail.com>
2023-09-29 23:50:39 -0700

    update some comments

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -1,67 +1,80 @@
 describe("Utilities test (with setup and tear-down)", function() {
     beforeEach(function () {
+      // Set default values for bill amount and tip amount, and submit a payment before each test
       billAmtInput.value = 100;
       tipAmtInput.value = 20;
       submitPaymentInfo();
     });
   
     it('should sum total tip amount of all payments on sumPaymentTotal()', function () {
+      // Test whether 'sumPaymentTotal' correctly calculates the total tip amount of all payments
       expect(sumPaymentTotal('tipAmt')).toEqual(20);
   
+      // Update input values and submit another payment
       billAmtInput.value = 200;
       tipAmtInput.value = 40;
-  
       submitPaymentInfo();
   
+      // Expectations for the test case
       expect(sumPaymentTotal('tipAmt')).toEqual(60);
     });
   
     it('should sum total bill amount of all payments on sumPaymentTotal()', function () {
+      // Test whether 'sumPaymentTotal' correctly calculates the total bill amount of all payments
       expect(sumPaymentTotal('billAmt')).toEqual(100);
   
+      // Update input values and submit another payment
       billAmtInput.value = 200;
       tipAmtInput.value = 40;
-  
       submitPaymentInfo();
   
+      // Expectations for the test case
       expect(sumPaymentTotal('billAmt')).toEqual(300);
     });
   
     it('should sum total tip percent on sumPaymentTotal()', function () {
+      // Test whether 'sumPaymentTotal' correctly calculates the total tip percentage of all payments
       expect(sumPaymentTotal('tipPercent')).toEqual(20);
   
+      // Update input values and submit another payment
       billAmtInput.value = 100;
       tipAmtInput.value = 20;
-  
       submitPaymentInfo();
   
+      // Expectations for the test case
       expect(sumPaymentTotal('tipPercent')).toEqual(40);
     });
   
     it('should sum tip percent of a single tip on calculateTipPercent()', function () {
+      // Test whether 'calculateTipPercent' correctly calculates the tip percentage for a given bill and tip amount
       expect(calculateTipPercent(100, 23)).toEqual(23);
       expect(calculateTipPercent(111, 11)).toEqual(10);
     });
   
-    it('should generate new td from value and append to tr on appendTd(tr, value)', function () {
+    it('should generate new td from value and append to tr on appendTd (tr, value)', function () {
+      // Test whether 'appendTd' correctly generates a new 'td' element with the provided value and appends it to the specified 'tr'
       let newTr = document.createElement('tr');
   
       appendTd(newTr, 'test');
   
+      // Expectations for the test case
       expect(newTr.children.length).toEqual(1);
       expect(newTr.firstChild.innerHTML).toEqual('test');
     });
   
-    it('should generate delete td and append to tr on appendDeleteBtn(tr, type)', function () {
+    it('should generate delete td and append to tr on appendDeleteBtn (tr, type)', function () {
+      // Test whether 'appendDeleteBtn' correctly generates a delete 'td' element ('X') and appends it to the specified 'tr'
       let newTr = document.createElement('tr');
   
       appendDeleteBtn(newTr);
   
+      // Expectations for the test case
       expect(newTr.children.length).toEqual(1);
       expect(newTr.firstChild.innerHTML).toEqual('X');
     });
   
     afterEach(function() {
+      // Reset input values and clear table data after each test
       billAmtInput.value = '';
       tipAmtInput.value = '';
       paymentTbody.innerHTML = '';
@@ -73,3 +86,4 @@ describe("Utilities test (with setup and tear-down)", function() {
       paymentId = 0;
     });
   });
+  
--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -1,67 +1,80 @@
 describe("Payments test (with setup and tear-down)", function() {
-    beforeEach(function () {
-      billAmtInput.value = 100;
-      tipAmtInput.value = 20;
-    });
-  
-    it('should add a new payment to allPayments on submitPaymentInfo()', function () {
-      submitPaymentInfo();
-  
-      expect(Object.keys(allPayments).length).toEqual(1);
-      expect(allPayments['payment1'].billAmt).toEqual('100');
-      expect(allPayments['payment1'].tipAmt).toEqual('20');
-      expect(allPayments['payment1'].tipPercent).toEqual(20);
-    });
-  
-    it('should not add a new payment on submitPaymentInfo() with empty input', function () {
-      billAmtInput.value = '';
-      submitPaymentInfo();
-  
-      expect(Object.keys(allPayments).length).toEqual(0);
-    });
-  
-    it('should payment update #paymentTable on appendPaymentTable()', function () {
-      let curPayment = createCurPayment();
-      allPayments['payment1'] = curPayment;
-  
-      appendPaymentTable(curPayment);
-  
-      let curTdList = document.querySelectorAll('#paymentTable tbody tr td');
-  
-      expect(curTdList.length).toEqual(4);
-      expect(curTdList[0].innerText).toEqual('$100');
-      expect(curTdList[1].innerText).toEqual('$20');
-      expect(curTdList[2].innerText).toEqual('20%');
-      expect(curTdList[3].innerText).toEqual('X');
-    });
-  
-    it('should create a new payment on createCurPayment()', function () {
-      let expectedPayment = {
-        billAmt: '100',
-        tipAmt: '20',
-        tipPercent: 20,
-      }
-  
-      expect(createCurPayment()).toEqual(expectedPayment);
-    });
-  
-    it('should not create payment with empty input on createCurPayment()', function () {
-      billAmtInput.value = '';
-      tipAmtInput.value = '';
-      let curPayment = createCurPayment();
-  
-      expect(curPayment).toEqual(undefined);
-    });
-  
-    afterEach(function() {
-      billAmtInput.value = '';
-      tipAmtInput.value = '';
-      paymentTbody.innerHTML = '';
-      summaryTds[0].innerHTML = '';
-      summaryTds[1].innerHTML = '';
-      summaryTds[2].innerHTML = '';
-      serverTbody.innerHTML = '';
-      paymentId = 0;
-      allPayments = {};
-    });
+  beforeEach(function () {
+    // Set default values for bill amount and tip amount before each test
+    billAmtInput.value = 100;
+    tipAmtInput.value = 20;
   });
+
+  it('should add a new payment to allPayments on submitPaymentInfo()', function () {
+    // Test whether a new payment is correctly added to 'allPayments' when 'submitPaymentInfo' is called
+    submitPaymentInfo();
+
+    // Expectations for the test case
+    expect(Object.keys(allPayments).length).toEqual(1);
+    expect(allPayments['payment1'].billAmt).toEqual('100');
+    expect(allPayments['payment1'].tipAmt).toEqual('20');
+    expect(allPayments['payment1'].tipPercent).toEqual(20);
+  });
+
+  it('should not add a new payment on submitPaymentInfo() with empty input', function () {
+    // Test whether no new payment is added to 'allPayments' when 'submitPaymentInfo' is called with empty input
+    billAmtInput.value = '';
+    submitPaymentInfo();
+
+    // Expectations for the test case
+    expect(Object.keys(allPayments).length).toEqual(0);
+  });
+
+  it('should payment update #paymentTable on appendPaymentTable()', function () {
+    // Test whether 'appendPaymentTable' correctly updates the payment table with payment details
+    let curPayment = createCurPayment();
+    allPayments['payment1'] = curPayment;
+
+    appendPaymentTable(curPayment);
+
+    // Select the updated table data cells
+    let curTdList = document.querySelectorAll('#paymentTable tbody tr td');
+
+    // Expectations for the test case
+    expect(curTdList.length).toEqual(4);
+    expect(curTdList[0].innerText).toEqual('$100');
+    expect(curTdList[1].innerText).toEqual('$20');
+    expect(curTdList[2].innerText).toEqual('20%');
+    expect(curTdList[3].innerText).toEqual('X');
+  });
+
+  it('should create a new payment on createCurPayment()', function () {
+    // Test whether 'createCurPayment' correctly creates a new payment object
+    let expectedPayment = {
+      billAmt: '100',
+      tipAmt: '20',
+      tipPercent: 20,
+    }
+
+    // Expectations for the test case
+    expect(createCurPayment()).toEqual(expectedPayment);
+  });
+
+  it('should not create payment with empty input on createCurPayment()', function () {
+    // Test whether 'createCurPayment' returns undefined when called with empty input
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
+    let curPayment = createCurPayment();
+
+    // Expectations for the test case
+    expect(curPayment).toEqual(undefined);
+  });
+
+  afterEach(function() {
+    // Reset input values and clear table data after each test
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
+    paymentTbody.innerHTML = '';
+    summaryTds[0].innerHTML = '';
+    summaryTds[1].innerHTML = '';
+    summaryTds[2].innerHTML = '';
+    serverTbody.innerHTML = '';
+    paymentId = 0;
+    allPayments = {};
+  });
+});
